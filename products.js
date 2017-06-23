44e335ea7e352720341be933403a9243808a961a
Anton <SilverSilk@ukr.net>
2017-06-23 23:19:04 +0300

    MobileJS 23_06

---
+++ products.js
@@ -0,0 +1,81 @@
+var phones = [{
+    "id": 1002,
+    "productName": "Xiaomi Redmi 4 Prime 3/32GB Gold",
+    "priceUAH": 4349,
+    "brandName": "Xiaomi",
+    "countProducts": 3,
+    "operationSystem": "Android",
+    "numSimCard": 2,
+    "productRate": 5,
+    "countViews": 5236
+}, {
+    "id": 1003,
+    "productName": "Xiaomi Redmi 4X 2/16GB Black",
+    "priceUAH": 3699,
+    "brandName": "Xiaomi",
+    "countProducts": 1,
+    "operationSystem": "Android",
+    "numSimCard": 2,
+    "productRate": 4.5,
+    "countViews": 785
+}, {
+    "id": 1004,
+    "productName": "Xiaomi Redmi 4X 3/32GB Gold",
+    "priceUAH": 4199,
+    "brandName": "Xiaomi",
+    "countProducts": 0,
+    "operationSystem": "Android",
+    "numSimCard": 2,
+    "productRate": 4.8,
+    "countViews": 453
+}, {
+    "id": 1005,
+    "productName": "Samsung Galaxy J1 2016 SM-J120H ",
+    "priceUAH": 2799,
+    "brandName": "Samsung",
+    "countProducts": 3,
+    "operationSystem": "Android",
+    "numSimCard": 2,
+    "productRate": 3,
+    "countViews": 1230
+}, {
+    "id": 1006,
+    "productName": "Samsung Galaxy J7 J700H/DS Black",
+    "priceUAH": 4999,
+    "brandName": "Samsung",
+    "countProducts": 2,
+    "operationSystem": "Android",
+    "numSimCard": 1,
+    "productRate": 4,
+    "countViews": 4532
+}, {
+    "id": 1007,
+    "productName": "Samsung Galaxy J5 Prime G570F/DS",
+    "priceUAH": 5199,
+    "brandName": "Samsung",
+    "countProducts": 2,
+    "operationSystem": "Android",
+    "numSimCard": 2,
+    "productRate": 4.1,
+    "countViews": 1287
+}, {
+    "id": 1008,
+    "productName": "Apple iPhone 7 Plus 128GB Black",
+    "priceUAH": 28999,
+    "brandName": "Apple",
+    "countProducts": 1,
+    "operationSystem": "iOS",
+    "numSimCard": 1,
+    "productRate": 4.1,
+    "countViews": 1222
+}, {
+    "id": 1009,
+    "productName": "Apple iPhone 7 256GB Jet Black",
+    "priceUAH": 26999,
+    "brandName": "Apple",
+    "countProducts": 2,
+    "operationSystem": "iOS",
+    "numSimCard": 2,
+    "productRate": 4.4,
+    "countViews": 999
+}];
